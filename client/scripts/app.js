15ba2b0f0dc42e33e85e0b9c949e0c0e481a99e3
Christopher Treviño <christophertrev@gmail.com>
2015-02-28 14:44:19 -0800

    add index.htnl and some firebase

---
+++ client/scripts/app.js
@@ -0,0 +1,7 @@
+// var myFirebaseRef = new Firebase("https://<your-firebase>.firebaseio.com/");
+// var myFirebaseRef = new Firebase("https://<your-firebase>.firebaseio.com/");
+
+
+
+var app = angular.module("sampleApp", ["firebase"]);
+
---
+++ server/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
+
+
+
+
+var app = express(); 
+
+app.use(morgan('dev'));
+
+app.use(express.static(path.join(__dirname + '/../client')));
+
+
+app.set('port', (process.env.PORT || 3000))
+
+var server = app.listen(app.get('port'), function () {
+
+  var host = server.address().address
+  var port = server.address().port
+  console.log('Example app listening at http://%s:%s', host, port)
+
+})
