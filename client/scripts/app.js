3432d87fb263a4cac9b937f4f0f9cd6b9febcfd2
Christopher Treviño <christophertrev@gmail.com>
2015-02-28 15:32:03 -0800

    firebase works

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,5 +3,27 @@
 
 
 
-var app = angular.module("sampleApp", ["firebase"]);
+var app = angular.module("worstCity", ["firebase"]);
 
+app.controller("SampleCtrl", ["$scope", "$firebase",
+  function($scope, $firebase) {
+  var ref = new Firebase("https://publicdata-transit.firebaseio.com/sf-muni");
+  // var ref = new Firebase("  https://publicdata-parking.firebaseio.com/");
+  // var ref = new Firebase("https://vivid-fire-4083.firebaseio.com/");
+  // var 
+
+  // create an AngularFire reference to the data
+  // console.log(ref)
+  var sync = $firebase(ref);
+  // var sync = $firebase(ref.child('routes').child('N'));
+  // console.log(sync.$asObject())
+  sync.$asObject().$bindTo($scope, "data");
+  // download the data into a local object
+  // $scope.transitData = sync.$asObject();
+
+  // $scope.data.child('routes');
+  // console.log($scope.data)
+  // console.log(Object.keys($scope.data))
+
+}
+]);
